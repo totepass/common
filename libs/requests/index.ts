6a37181917c6d76aab4aba55410c5b6cd02ee2c2
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2023-12-25 18:06:50 +0100

    Fix error handling

--- libs/requests/index.ts
+++ libs/requests/index.ts
@@ -1,49 +1,57 @@
 export type RequestOptions = {
-	json?: boolean;
-}
+  json?: boolean;
+  browser?: boolean;
+};
 
 export async function request(
-	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-	url: string,
-	body?: any,
-	headers?: { [key: string]: string },
-	options?: RequestOptions
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  url: string,
+  body?: any,
+  headers?: { [key: string]: string },
+  options?: RequestOptions
 ) {
-	if(options?.json) {
-		headers = {
-			...headers,
-			'Content-Type': 'application/json',
-			'Accept': 'application/json'
-		}
-		body = JSON.stringify(body);
-	}
+  if (options?.json) {
+    headers = {
+      ...headers,
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    };
+    body = JSON.stringify(body);
+  }
 
-	const response = await fetch(url, {
-		method: method,
-		body: body,
-		headers: headers
-	});
+  try {
+    const response = await fetch(url, {
+      method: method,
+      body: body,
+      headers: headers,
+    });
 
-	// Check if the response is an HTTP status code, i.e. between 100 and 599
-	if (response.status < 100 || response.status > 599) {
-		alert('An error occurred while processing your request. Please try again later.');
-	}
+    // Check if the response is an HTTP status code, i.e. between 100 and 599
+    if (response.status < 100 || response.status > 599) {
+      throw new Error("Invalid HTTP status code");
+    }
 
-	return response;
+    return response;
+  } catch (err) {
+    if (!options || options.browser === undefined || options.browser === null || options.browser) {
+      alert("An error occurred while processing your request. Please try again later.");
+    }
+    throw err;
+  }
 }
 
 export async function get(url: string, headers?: { [key: string]: string }, options?: RequestOptions) {
-    return await request('GET', url, undefined, headers, options);
+  return request("GET", url, undefined, headers, options);
 }
 
 export async function post(url: string, body?: any, headers?: { [key: string]: string }, options?: RequestOptions) {
-    return await request('POST', url, body, headers, options);
+  return request("POST", url, body, headers, options);
 }
 
 export async function put(url: string, body?: any, headers?: { [key: string]: string }, options?: RequestOptions) {
-    return await request('PUT', url, body, headers, options);
+  return request("PUT", url, body, headers, options);
 }
 
 export async function del(url: string, headers?: { [key: string]: string }, options?: RequestOptions) {
-    return await request('DELETE', url, undefined, headers, options);
+  return request("DELETE", url, undefined, headers, options);
 }
