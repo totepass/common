a29cf296ca0f2fea7e0f43b32117a619f24812a8
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2023-12-29 12:38:47 +0100

    Add address type

---
+++ libs/types/geo.ts
@@ -0,0 +1,14 @@
+export type Address = {
+  id: string;
+  name: string;
+  coordinates: [number, number];
+  address: {
+    street: string;
+    number: string;
+    city: string;
+    country: string;
+    zip: string;
+  };
+  timezone: string;
+  context: { [key: string]: any };
+};
