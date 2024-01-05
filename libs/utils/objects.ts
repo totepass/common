08f1a76b085d19d1a661c987440c084b7b65c555
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2024-01-06 00:04:13 +0100

    Create trim strings functions

--- libs/utils/objects.ts
+++ libs/utils/objects.ts
@@ -10,3 +10,15 @@ export function getValueFromKeyString(object: any, key: string): string | null {
 
 	return getValueFromKeyString(object[firstKey], nextKey);
 }
+
+export function trimAllStrings(object: any): any {
+	if (typeof object === 'string') {
+		return object.trim();
+	}
+	if (typeof object === 'object') {
+		for (const key in object) {
+			object[key] = trimAllStrings(object[key]);
+		}
+	}
+	return object;
+}
