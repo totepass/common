3bb1b210cb592b7096ecbf3ab8ad75a0b00c1d33
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2024-01-06 00:25:39 +0100

    Implement trip UUID

--- libs/uuid/index.ts
+++ libs/uuid/index.ts
@@ -1,3 +1,4 @@
+import { randomInteger } from "../utils/numbers";
 
 /**
  * Generates a UUID. The UUID follows the format of year-month-day-randomHex.
@@ -13,4 +14,41 @@ export function UUID() {
     const random = Math.floor(Math.random() * 0xFFFFFFFF).toString(16).padStart(8, '0');
 
     return `${year}-${month}-${day}-${random}`;
+}
+
+/**
+ * Generates a UUID for a trip. The UUID follows the format of [A-Z][0-9A-Z][0-9]{3,6}
+ * @returns `string` The generated UUID.
+ */
+export function tripUUID() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lettersNumbers = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const badWords = [
+        "AF",
+        "BJ",
+        "BW",
+        "CK",
+        "CM",
+        "CU",
+        "FG",
+        "FK",
+        "GY",
+        "HP",
+        "KK",
+        "NG",
+        "PN",
+        "PT",
+        "SH",
+    ] 
+
+    let tripCarrier = "";
+    do {
+        tripCarrier = letters[randomInteger(0, letters.length - 1)];
+        tripCarrier += lettersNumbers[randomInteger(0, lettersNumbers.length - 1)];
+    } while (badWords.includes(tripCarrier));
+
+    let tripNumber = randomInteger(0, 999999).toString().padStart(6, '0');
+
+    return `${tripCarrier}${tripNumber}`;
 }
