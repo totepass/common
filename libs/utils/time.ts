4346fd375949dda6e6df4f076ecc50146e626467
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2024-01-10 23:49:18 +0100

    Fix string

--- libs/utils/time.ts
+++ libs/utils/time.ts
@@ -57,25 +57,25 @@ export function durationAsString(
     let years = Math.floor(duration / 525600);
 
     if (options.years) {
-        output += years + "y ";
+        output += years + "years ";
         duration -= years * 525600;
     }
 
     let months = Math.floor(duration / 43800);
     if (options.months) {
-        output += months + "mo ";
+        output += months + "months ";
         duration -= months * 43800;
     }
 
     let weeks = Math.floor(duration / 10080);
     if (options.weeks) {
-        output += weeks + "w ";
+        output += weeks + "weeks ";
         duration -= weeks * 10080;
     }
 
     let days = Math.floor(duration / 1440);
     if (options.days) {
-        output += days + "d ";
+        output += days + "days ";
         duration -= days * 1440;
     }
 
