4e688f647f64b4eca7413f243a758b70b26b5eb9
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2024-01-10 23:46:04 +0100

    Handle more components of time

--- libs/utils/time.ts
+++ libs/utils/time.ts
@@ -16,11 +16,72 @@ export function calculateDuration(start: DT, end: DT): number {
     return endDate.diff(startDate).as("minutes");
 }
 
-export function durationAsString(duration: number): string {
-    const hours = Math.floor(duration / 60);
-    const minutes = duration % 60;
+/**
+ * Convert a duration in minutes to a string
+ * @param duration `number` Duration in minutes
+ * @param options `object` Options for the output string
+ * @param options.years `boolean` Include years in the output string
+ * @param options.months `boolean` Include months in the output string
+ * @param options.weeks `boolean` Include weeks in the output string
+ * @param options.days `boolean` Include days in the output string
+ * @param options.hours `boolean` Include hours in the output string
+ * @param options.minutes `boolean` Include minutes in the output string
+ * @param options.seconds `boolean` Include seconds in the output string
+ * @returns `string` Duration as a string
+ */
+export function durationAsString(duration: number, options: {years: boolean; months: boolean; weeks: boolean; days: boolean; hours: boolean; minutes: boolean; seconds: boolean} = {
+    years: false,
+    months: false,
+    weeks: false,
+    days: false,
+    hours: true,
+    minutes: true,
+    seconds: false
+}): string {
+    let output = "";
+    let years = Math.floor(duration / 525600);
 
-    return `${hours}h ${minutes}m`;
+    if (options.years) {
+        output += years + "y ";
+        duration -= years * 525600;
+    }
+
+    let months = Math.floor(duration / 43800);
+    if (options.months) {
+        output += months + "mo ";
+        duration -= months * 43800;
+    }
+
+    let weeks = Math.floor(duration / 10080);
+    if (options.weeks) {
+        output += weeks + "w ";
+        duration -= weeks * 10080;
+    }
+
+    let days = Math.floor(duration / 1440);
+    if (options.days) {
+        output += days + "d ";
+        duration -= days * 1440;
+    }
+
+    let hours = Math.floor(duration / 60);
+    if (options.hours) {
+        output += hours + "h ";
+        duration -= hours * 60;
+    }
+
+    let minutes = Math.floor(duration);
+    if (options.minutes) {
+        output += minutes + "m ";
+        duration -= minutes;
+    }
+
+    let seconds = Math.floor(duration * 60);
+    if (options.seconds) {
+        output += seconds + "s ";
+    }
+
+    return output.trim();
 }
 
 /**
