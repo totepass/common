088130ccd26ef7a04a86ceecad5110a75b2ac5e9
Álvaro Galisteo <8809764+SrGMC@users.noreply.github.com>
2024-03-03 22:14:15 +0100

    Limit data

--- libs/types/user.ts
+++ libs/types/user.ts
@@ -1,31 +1,32 @@
 export type User = {
-  id: string;
-  address?: {
-    address: string;
-    city: string;
-    countryCode: string;
-    coordinates: [number, number];
-  };
-  avatar?: string;
-  email: string;
-  invite?: string;
-  language: "es" | "en" | string;
-  name: string;
-  surname: string;
-  password?: string;
-  passwordConfirm?: string;
-  verified: boolean;
-  calendarId?: string;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt?: Date | null;
+    id: string;
+    email: string;
+    language: "es" | "en" | string;
+    name: string;
+    surname: string;
+
+    address?: {
+        address: string;
+        city: string;
+        countryCode: string;
+        coordinates: [number, number];
+    };
+    avatar?: string;
+    invite?: string;
+    password?: string;
+    passwordConfirm?: string;
+    verified?: boolean;
+    calendarId?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    deletedAt?: Date | null;
 };
 
 export type Session = {
-  id: number;
-  userId: string;
-  token: string;
-  refreshToken: string;
-  expiresAt: Date;
-  createdAt: Date;
+    id: number;
+    userId: string;
+    token: string;
+    refreshToken: string;
+    expiresAt: Date;
+    createdAt: Date;
 };
